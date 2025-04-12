b0b749b6e4dc486f7944493dcfe2ba48fd27dd4d
Saurav Kumar <saurav.0kmr@gmail.com>
2025-04-12 18:26:59 +0530

    Saved blogs and blog details screen

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -115,6 +115,17 @@ def generate_blog_from_transcription(transcription):
     generated_content = response.choices[0].message.content
     return generated_content
 
+def blog_list(request):
+    blog_articles = BlogPost.objects.filter(user=request.user)
+    return render(request, "all-blogs.html", {'blog_articles': blog_articles})
+
+def blog_details(request, pk):
+    blog_article_detail = BlogPost.objects.get(id=pk)
+    if request.user == blog_article_detail.user:
+        return render(request, 'blog-details.html', {'blog_article_detail': blog_article_detail})
+    else:
+        return redirect('/')
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
